15a2f062edfb056f6b85970c51e74e666fdcab4e
loverfish <blackmemberb@gmail.com>
2021-02-16 17:47:08 +0300

    Added ObjectDetailMixin + modified tags_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
 
 from .models import Post, Tag
+from .utlils import ObjectDetailMixin
 
 
 def posts_list(request):
@@ -8,9 +11,14 @@ def posts_list(request):
     return render(request, 'blog/home.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
 
 
 def tags_list(request):
@@ -18,6 +26,10 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+# def tag_detail(request, slug):
+#     tag = Tag.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+# def post_detail(request, slug):
+#     post = Post.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/post_detail.html', context={'post': post})
