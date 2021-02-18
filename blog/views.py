61ce5df63cb115efd0ac489da06603981a4099fc
loverfish <blackmemberb@gmail.com>
2021-02-18 19:17:05 +0300

    Added Tagform + template tag_create

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.views.generic import View, CreateView
+from django.urls import reverse_lazy
 
 from .models import Post, Tag
+from .forms import TagForm
 from .utlils import ObjectDetailMixin
-
-
-def posts_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/home.html', context={'posts': posts})
 
 
 class PostDetail(ObjectDetailMixin, View):
@@ -19,6 +15,26 @@ class PostDetail(ObjectDetailMixin, View):
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
+
+class TagCreate(View):
+
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
+
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', context={'posts': posts})
 
 
 def tags_list(request):
